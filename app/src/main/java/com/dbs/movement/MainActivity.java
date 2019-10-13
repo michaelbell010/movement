938f0aade605574621c67b4377d299b7f2acf626
Michael Bell <10387336@mydbs.ie>
2019-10-13 17:22:22 +0100

    changed packed name

--- app/src/main/java/com/dbs/movement/MainActivity.java
+++ app/src/main/java/com/dbs/movement/MainActivity.java
@@ -1,7 +1,9 @@
 package com.dbs.movement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +12,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dbs.movement.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,26 +32,36 @@ public class MainActivity extends AppCompatActivity {
         emailId = findViewById(R.id.editText2);
         password = findViewById(R.id.editText);
         btnSignUp = findViewById(R.id.button);
-        btnSignUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String email = emailId.getText().toString();
-                String pwd = password.getText().toString();
-                if(email.isEmpty()){
-                    emailId.setError("Please enter email address");
-                    emailId.requestFocus();
+        btnSignUp.setOnClickListener(view -> {
+            String email = emailId.getText().toString();
+            String pwd = password.getText().toString();
+            if(email.isEmpty()){
+                emailId.setError("Please enter email address");
+                emailId.requestFocus();
 
-                } else if (pwd.isEmpty()) {
+            } else if (pwd.isEmpty()) {
 
-                    password.setError("Please enter password");
-                    password.requestFocus();
+                password.setError("Please enter password");
+                password.requestFocus();
 
-                }
-                else if(emailId.isEmpty() && pwd.isEmpty()){
-                    Toast.makeText( context: MainActivity.this, text "fields are empty!",Toast.LENGTH_SHORT).show();
-                }
+            }
+            else if(email.isEmpty() && pwd.isEmpty())
+            {
+                Toast.makeText(MainActivity.this, "fields are empty!",Toast.LENGTH_SHORT).show();
+            }
+            else if(!(email.isEmpty() && pwd.isEmpty())){
+                mFirebaseAuth.createUserWithEmailAndPassword(email, pwd).addOnCompleteListener(MainActivity.this, task -> {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Signup Unsuccessful!", Toast.LENGTH_SHORT).show();
+
+                    } else {
+                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                    }
+                });
+            }
+            else {
+                Toast.makeText(MainActivity.this, "Error Ocurred!", Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 }
